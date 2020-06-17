d3b1731517b5a5e14e9e1630b40981bcf72ed3b1
NickolasDzR <nikolyae@mail.ru>
2020-06-17 17:08:34 +0300

    some new updates

--- src/blocks/modules/mini-slider/mini-slider.js
+++ src/blocks/modules/mini-slider/mini-slider.js
@@ -1,21 +1,23 @@
 import Swiper from "swiper";
 
 
-new Swiper(".mini-slider", {
-    slidesPerView: 1,
-    draggable: true,
-    speed: 1000,
-    pagination: {
-        el: ".mini-slider__pagination",
-        type: "fraction",
-        renderFraction: function (currentClass, totalClass) {
-            return "<span class=\"" + currentClass + "\"></span>" +
-        " ИЗ " +
-        "<span class=\"" + totalClass + "\"></span>";
+if (document.querySelector(".mini-slider")) {
+    new Swiper(".mini-slider", {
+        slidesPerView: 1,
+        draggable: true,
+        speed: 1000,
+        pagination: {
+            el: ".mini-slider__pagination",
+            type: "fraction",
+            renderFraction: function (currentClass, totalClass) {
+                return "<span class=\"" + currentClass + "\"></span>" +
+          " ИЗ " +
+          "<span class=\"" + totalClass + "\"></span>";
+            },
         },
-    },
-    navigation: {
-        nextEl: ".mini-slider__arrow_next",
-        prevEl: ".mini-slider__arrow_prev",
-    },
-});
+        navigation: {
+            nextEl: ".mini-slider__arrow_next",
+            prevEl: ".mini-slider__arrow_prev",
+        },
+    });
+}
--- src/blocks/modules/ticker/ticker.js
+++ src/blocks/modules/ticker/ticker.js
@@ -1,14 +1,15 @@
 import Swiper from "swiper";
 
-new Swiper(".ticker", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    // slidesPerView: 'auto',
-    freeMode: true,
-    loop: true,
-    autoplay: {
-        delay: 0,
-    },
-    speed: 10000,
-    allowTouchMove: false
-});
+if (document.querySelector("ticker")) {
+    new Swiper(".ticker", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 0,
+        },
+        speed: 10000,
+        allowTouchMove: false
+    });
+}
