d2aa42bfcdda512caa774f85da5d78de75699882
NickolasDzR <nikolyae@mail.ru>
2020-06-17 16:36:26 +0300

    footer in 404

--- src/blocks/modules/top-slider/top-slider.js
+++ src/blocks/modules/top-slider/top-slider.js
@@ -1,58 +1,62 @@
-import Swiper from 'swiper';
+import Swiper from "swiper";
 import helpers from "../../../js/import/components";
 
 const rem = helpers.rem;
 
 
-let topSliderThumbs = new Swiper(".top-slider__slider", {
-  slidesPerView: 5,
-  draggable: true,
-  speed: 700,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-});
+let topSliderThumbs = undefined;
+if (document.querySelector(".top-slider__slider")) {
+    topSliderThumbs = new Swiper(".top-slider__slider", {
+        slidesPerView: 5,
+        draggable: true,
+        speed: 700,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+}
 
+if (document.querySelector(".slider-background")) {
+    const backSlider = new Swiper(".slider-background", {
+        slidesPerView: 1,
+        draggable: false,
+        allowTouchMove: false,
+        speed: 700,
+        effect: "fade",
+        loop: true,
+        thumbs: {
+            swiper: topSliderThumbs,
+        },
+        pagination: {
+            el: ".top-slider__pagination",
+            type: "fraction",
+            renderFraction: function (currentClass, totalClass) {
+                return "<span class=\"" + currentClass + "\"></span>" +
+          " ИЗ " +
+          "<span class=\"" + totalClass + "\"></span> ФОТО";
+            },
+        },
+        navigation: {
+            nextEl: ".top-slider__arrow_next",
+            prevEl: ".top-slider__arrow_prev",
+        },
+        init: false
+    });
 
-const backSlider = new Swiper(".slider-background", {
-  slidesPerView: 1,
-  draggable: false,
-  allowTouchMove: false,
-  speed: 700,
-  effect: "fade",
-  loop: true,
-  thumbs: {
-    swiper: topSliderThumbs,
-  },
-  pagination: {
-    el: ".top-slider__pagination",
-    type: "fraction",
-    renderFraction: function (currentClass, totalClass) {
-        return '<span class="' + currentClass + '"></span>' +
-                ' ИЗ ' +
-                '<span class="' + totalClass + '"></span> ФОТО';
-    },
-  },
-  navigation: {
-    nextEl: ".top-slider__arrow_next",
-    prevEl: ".top-slider__arrow_prev",
-  },
-  init: false
-});
+    backSlider.on("init slideChange resize", function () {
+        const slider = document.querySelector(".top-slider__slider"),
+            slideActive = slider.querySelector(".swiper-slide-thumb-active"),
+            slideOffset = slideActive.swiperSlideOffset,
+            slideBorder = slider.querySelector(".swiper-border"),
+            slideHeight = slideActive.offsetHeight,
+            slideWidth = slideActive.offsetWidth;
+  
+        slideBorder.style.transform = `translateX(${rem(slideOffset)})`;
+        slideBorder.style.height = rem(slideHeight);
+        slideBorder.style.width = rem(slideWidth);
+    });
+  
+  
+    backSlider.init();
 
-
-backSlider.on("init slideChange resize", function () {
-  const slider      = document.querySelector(".top-slider__slider"),
-        slideActive = slider.querySelector('.swiper-slide-thumb-active'),
-        slideOffset = slideActive.swiperSlideOffset,
-        slideBorder = slider.querySelector('.swiper-border'),
-        slideHeight = slideActive.offsetHeight,
-        slideWidth  = slideActive.offsetWidth;
-
-  slideBorder.style.transform = `translateX(${rem(slideOffset)})`;
-  slideBorder.style.height    = rem(slideHeight);
-  slideBorder.style.width     = rem(slideWidth);
-});
-
-
-backSlider.init();
+}
